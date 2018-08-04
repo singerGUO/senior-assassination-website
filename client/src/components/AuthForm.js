f4cb0f96d66eb090c55b3494c434e49fa70429b7
aaronlam <aaronlam1618@gmail.com>
2018-08-04 10:16:58 -0700

    sign up page, sign in page, and welcome page built

---
+++ client/src/components/AuthForm.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+export default class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+      profileImageUrl: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const authType = this.props.signUp ? "signUp" : "signIn";
+    this.props.onAuth(authType, this.state).then(() => {
+      console.log("LOGGED IN SUCCESSFULLY");
+    })
+  };
+
+  render() {
+    const { email, username, password, profileImageUrl } = this.state;
+    const { heading, buttonText, signUp } = this.props;
+    return (
+      <div>
+        <div className="row justify-content-md-center text-center">
+          <div className="col-md-6">
+            <form onSubmit={this.handleSubmit}>
+              <h2>{heading}</h2>
+              <label htmlFor="email">Email:</label>
+              <input
+                className="form-control"
+                id="email"
+                name="email"
+                onChange={this.handleChange}
+                value={email}
+                type="text"
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                className="form-control"
+                id="password"
+                name="password"
+                onChange={this.handleChange}
+                type="password"
+              />
+              {signUp && (
+                <div>
+                  <label htmlFor="username">Username:</label>
+                  <input
+                    className="form-control"
+                    id="username"
+                    name="username"
+                    onChange={this.handleChange}
+                    value={username}
+                    type="text"
+                  />
+                  <label htmlFor="image-url">Image URL:</label>
+                  <input
+                    className="form-control"
+                    id="image-url"
+                    name="profileImageUrl"
+                    onChange={this.handleChange}
+                    type="text"
+                    value={profileImageUrl}
+                  />
+                </div>
+              )}
+              <button
+                type="submit"
+                className="btn btn-primary btn-block btn-lg"
+              >
+                {buttonText}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ client/src/containers/Navbar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Logo from "../images/warbler-logo.png";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            <img src={Logo} alt="Home" />
+          </Link>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <Link to="/signup">Sign up</Link>
+            </li>
+            <li>
+              <Link to="/signin">Log in</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser
+  };
+}
+
+export default connect(mapStateToProps, null)(Navbar);
